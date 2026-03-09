112743be08568f4932907b75284648f503ff2074
Lucas TURCHESCHI <lucas.turcheschi@isen.yncrea.fr>
2026-03-09 16:11:05 +0100

    Feat : login, signup and universe

--- app/src/main/java/fr/isen/segfault/thedisneyapp/UniversesActivity.kt
+++ app/src/main/java/fr/isen/segfault/thedisneyapp/UniversesActivity.kt
@@ -29,19 +29,3 @@ class UniversesActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    MyDisneyAppTheme {
-        Greeting2("Android")
-    }
-}
